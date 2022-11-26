a5641eb96c1e859db1c44f79c8d2763da75f4e19
Rahmouni Moulay <moulay.rahmouni1@gmail.Com>
2022-11-26 20:52:49 +0100

    nergyal

---
+++ nergyal/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      white: "#fff",
+      black: "#111",
+      blue: "#0077BB",
+      orange: "#FC5900",
+      grey: "#1f1f1f",
+    },
+    extend: {},
+  },
+  plugins: [],
+}
