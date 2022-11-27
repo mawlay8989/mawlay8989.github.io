3432a5e19856c4fe4563d37a1ae27b720c2e2407
Rahmouni Moulay <moulay.rahmouni1@gmail.Com>
2022-11-27 15:35:19 +0100

    nergyal

--- nergyal/file.js
+++ nergyal/file.js
@@ -17,3 +17,32 @@ function scrollDown() {
 arrowDown.addEventListener('click', function() {
     toScroll.scrollIntoView();
 });
+
+document.addEventListener("scroll", handleScroll);
+
+let fab = document.querySelector('#fab');
+
+      function handleScroll()
+      {
+        let scrollableHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+        let gr = 0.40;
+
+        if ((document.documentElement.scrollTop / scrollableHeight ) > gr) {
+          if(!fab.classList.contains("fab-fadein"))
+            fab.classList.add("fab-fadein")
+          } else {
+            //hide button
+            if(fab.classList.contains("fab-fadein"))
+            fab.classList.remove("fab-fadein")
+          }
+      }
+
+      fab.addEventListener("click", scrollToTop);
+
+      function scrollToTop()
+      {
+        window.scrollTo({
+          top: 0,
+          behavior: "smooth",
+        });
+      }
